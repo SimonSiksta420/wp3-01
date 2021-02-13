6d27e2c72fad8a689366f94549fc7b4dcd9cc0f9
SimonSiksta420 <60382022+SimonSiksta420@users.noreply.github.com>
2021-02-13 18:44:04 +0100

    Add files via upload
    J'aime les chèvres

---
+++ functions.php
@@ -0,0 +1,30 @@
+<?php
+function imbroke():void {
+
+$caryear = 3000;
+
+switch ($caryear) {
+
+case $caryear <= 1960 :
+echo "You have a great taste grandpa.";
+break;
+
+case $caryear <= 1990 :
+echo "Lada is working when i hit it with hammer.";
+break;
+
+case $caryear <= 2000 :
+echo "Opel Corsa 111 Deutsche qualitat.";
+break;
+
+case $caryear <= 2010 :
+echo "You have family and your wife hates you.";
+break;
+
+default: echo "Future man with flying car. Oppresor nigger.";
+
+} }
+
+
+
+?>
---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+require_once('functions.php');
+?>
+
+<html>
+  <head>
+    <title>Cars</title>
+  </head>
+  <body>
+
+   <?php
+
+  imbroke()
+
+   ?>
+
+  </body>
+</html>
